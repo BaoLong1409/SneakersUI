a3b414f352d006526c9914235ddb4d1f8862cff3
Bảo Long <anhruoia1a1@gmail.com>
2025-09-17 15:56:03 +0700

    change link backend

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:44398/api/v1/',
+    apiUrl: 'https://sneakersbe-production.up.railway.app/api/v1/',
     webSocketUrl: "https://localhost:44398/chathub"
     // apiImage: 'https://localhost:8089/'
 };
